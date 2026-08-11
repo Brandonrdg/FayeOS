4b99ce89db55d8b4482b82f5dc078f328da837e4
Brandonrdg <bran24rodriguezabarca@gmail.com>
2026-08-10 22:16:17 -0600

    Project F.A.Y.E

---
+++ FayeOS/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace FayeOS
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
